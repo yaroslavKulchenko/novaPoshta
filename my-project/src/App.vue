d7e9dcf7c632a7b5d46fefc468e8db1a6e308c80
yaroslavKulchenko <perekatipole123123@gmail.com>
2020-11-02 02:45:38 +0300

    working project

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Нова пошта</h1>
+    <h1 class="ribbon">Нова пошта</h1>
    <br>
       <a>Введіть місто</a>
    <br>
@@ -8,11 +8,11 @@
    <br>
       <a>{{city}}</a>
    <br>
-   <button @click="q">Search</button>
+   <button @click="q">Пошук за містом</button>
    <br>
       <a>{{cRef}}</a>
    <br>
-   <button @click="otdel">Вітділи</button>
+   <button @click="otdel">Відділи</button>
    <br>
 
    <table v-for="el in otd" v-bind:key="el.Description">
@@ -52,26 +52,26 @@ data(){
   methods:{
     q:function(){
       for (var i = 0; i < this.cities.length; i++){
-        if (this.cities[i].Description == this.city){
-          this.ci = this.cities[i];
-          this.cRef = this.ci.Ref;
-        }
+      if (this.cities[i].Description == this.city){
+     this.ci = this.cities[i];
+     this.cRef = this.ci.Ref;
       }
+    }
     },
     otdel:function(){
       axios.post("https://api.novaposhta.ua/v2.0/json/",{
-        "modelName": "AddressGeneral",
-        "calledMethod": "getWarehouses",
-        "methodProperties": {
-            "CityRef": this.cRef
-        },
-        "apiKey": "9a557481f95094531372a9d1b55222c8"
-      }).then((response) =>{
-        console.log(response.data.data);
-        this.otd = response.data.data;
-      })
-    }
+    "modelName": "AddressGeneral",
+    "calledMethod": "getWarehouses",
+    "methodProperties": {
+    "CityRef": this.cRef
+    },
+    "apiKey": "9a557481f95094531372a9d1b55222c8"
+    }).then((response) =>{
+    console.log(response.data.data);
+    this.otd = response.data.data;
+    })
   }
+ }
 }
 </script>
 
@@ -80,8 +80,56 @@ data(){
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1.ribbon { 
+font-size: 16px !important; 
+position: relative; 
+background: #ff0000; 
+color: #fff; 
+text-align: center; 
+padding: 1em 2em;
+margin: 0 0 3em;
+} 
+h1.ribbon:before, h1.ribbon:after { 
+content: ""; 
+position: absolute; 
+display: block; 
+bottom: -1em; 
+border: 1.5em solid #bd0404; 
+z-index: -1; 
+} 
+h1.ribbon:before { 
+left: -2em; 
+border-right-width: 1.5em; 
+border-left-color: transparent; 
+} 
+h1.ribbon:after { 
+right: -2em; 
+border-left-width: 1.5em; 
+border-right-color: transparent; 
+} 
+h1.ribbon .ribbon-content:before, h1.ribbon .ribbon-content:after { 
+content: ""; 
+position: absolute; 
+display: block; 
+border-style: solid; 
+border-color: #bd0404 transparent transparent transparent; 
+bottom: -1em; 
+} 
+h1.ribbon .ribbon-content:before { 
+left: 0; 
+border-width: 1em 0 0 1em; 
+} 
+h1.ribbon .ribbon-content:after { 
+right: 0; 
+border-width: 1em 1em 0 0; 
+}
+
+td{
+  background: rgba(206, 40, 40, 0.26);
+  text-align: center;
 }
 </style>
